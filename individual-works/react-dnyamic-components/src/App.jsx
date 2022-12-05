2399c5f0f104855a75cafbf0a6671ea0df3884ff
B3bekin <bebekin.le.2020@skola.ssps.cz>
2022-12-05 15:17:37 +0100

    second commit

--- individual-works/react-dnyamic-components/src/App.jsx
+++ individual-works/react-dnyamic-components/src/App.jsx
@@ -26,12 +26,13 @@ function App() {
         </div>
       </div>
 
+    
       <div className='grid grid-cols-4 gap-20 '>
 
         <div className='bg-orange-400 w-96 h-96 rounded-3xl text-center mx-auto'>
           <div className='py-44 font-mono text-lg'>Components</div>
         </div>
-
+        
       </div>
 
     </div>
