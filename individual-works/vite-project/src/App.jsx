0e6ceb64b38b4459de14c5dec0d7c014f0af04b6
B3bekin <bebekin.le.2020@skola.ssps.cz>
2023-01-29 22:56:49 +0100

    tak asi někdy jindy

--- individual-works/vite-project/src/App.jsx
+++ individual-works/vite-project/src/App.jsx
@@ -1,19 +1,26 @@
 import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
 import axios from './request.jsx'
 
 function App() {
-  const [data, setdata] = useState(0)
+  const [data, setData] = useState([])
+  const [stuff, setStuff] = useState(true)
+  const [loading, setLoading] = useState
 
-  useEffect(() => {
-    first
+  loading = async () => {
 
-    return () => {
-      second
+    try {
+      loading = await axios.get('https://api.golemio.cz/v2/gtfs/routes?limit=30', headers: {
+        'x-access-token': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImJlYmVraW4ubGUuMjAyMEBza29sYS5zc3BzLmN6IiwiaWQiOjE2NDMsIm5hbWUiOm51bGwsInN1cm5hbWUiOm51bGwsImlhdCI6MTY3NDQ3NTM5OCwiZXhwIjoxMTY3NDQ3NTM5OCwiaXNzIjoiZ29sZW1pbyIsImp0aSI6IjkzNzA5NTU5LWExNzMtNDVlYS1hN2Y2LTU3NDBhNTE1ZTU0MSJ9.Mm8JUGAJlZ7Ed2fQoXRzlwlKvUni6VWURQzRROpvL_4'
+      }), 
+      
+      data = loading
+
+      setStuff(data)
+      
     }
-  }, [third])
+  }
 
+  useEffect(() => { loading() }, [])
 
   return (
     <div>
