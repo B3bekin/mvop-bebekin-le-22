4c0f34a1dabe51281453febb83416d80f1e68861
b3bekin <bebekin.le.2020@skola.ssps.cz>
2023-03-06 13:57:17 +0100

    first coomit again

---
+++ individual-works/SC-project/src/Header.jsx
@@ -0,0 +1,13 @@
+//import Picture from './pth/logos.png'
+
+function Header(){
+    return(
+        <div>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore, nobis?
+            <Picture/>
+        </div>
+    )
+
+}
+
+export default Header
